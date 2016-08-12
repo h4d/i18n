f91101d1ef83bc3ca8ae336ae2fb90124d079613
Miguel <miguel.mailbox@gmail.com>
2016-08-12 10:35:37 +0200

    Interface for date decorators.

---
+++ src/DateDecoratorInterface.php
@@ -0,0 +1,69 @@
+<?php
+
+
+namespace H4D\I18n;
+
+
+interface DateDecoratorInterface
+{
+
+    const DEFAULT_LOCALE = 'en_GB';
+
+    const FORMAT_ALIAS_TIMESTAMP = 'timestamp';
+    const FORMAT_ALIAS_TIME      = 'time';
+    const FORMAT_ALIAS_SHORTTIME = 'shortTime';
+    const FORMAT_ALIAS_DATE      = 'date';
+    const FORMAT_ALIAS_DATETIME  = 'dateTime';
+
+
+    /**
+     * @param \DateTime $date
+     * @param string $formatAlias
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getFormattedDate(\DateTime $date, $formatAlias, $locale = '');
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getDate(\DateTime $dateTime, $locale = '');
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getDateTime(\DateTime $dateTime, $locale = '');
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getTime(\DateTime $dateTime, $locale = '');
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getShortTime(\DateTime $dateTime, $locale = '');
+
+    /**
+     * @param \DateTime $dateTime
+     * @param string $locale
+     *
+     * @return string
+     */
+    public function getTimestamp(\DateTime $dateTime, $locale = '');
+
+
+}
